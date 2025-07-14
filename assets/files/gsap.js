bb70290957f8aab59683ca554e4db2486a81f6a7
Dani-8 <daniyalw6190@gmail.com>
2025-07-14 21:00:15 +0500

    udpated

---
+++ assets/files/gsap.js
@@ -0,0 +1,68 @@
+gsap.from('.main-content', {
+    opacity: 0,
+    duration: 50,
+
+
+})
+
+gsap.fromTo(".svg-cont", 
+    {y: -400},
+    {
+        y: 
+
+    }
+
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
