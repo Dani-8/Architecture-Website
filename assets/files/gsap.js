58d330084ec026fccc773397ac8d7a32c694d2ee
Dani-8 <daniyalw6190@gmail.com>
2025-07-14 23:56:03 +0500

    updated

--- assets/files/gsap.js
+++ assets/files/gsap.js
@@ -1,17 +1,27 @@
-var navBarTL = gsap.timeline()
+var mainContentText = gsap.timeline()
 
-navBarTL.from('.main-content', {
-    opacity: 0,
-    duration: 5,
+// mainContentText.from('.main-content', {
+//     // opacity: 0,
+//     // duration: 5,
 
+
+// })
+
+
+mainContentText.from(".headingLine", {
+    y: 150,
+    duration: 1,
+    opacity: 1
 
 })
+
+
 
 
 var svgImgTL = gsap.timeline()
 svgImgTL.fromTo(".svg-cont", 
     {
-        y: -400,
+        // y: -400,
         duration: .2,
         opacity: 0,
         scale: .6,
@@ -28,15 +38,15 @@ svgImgTL.fromTo(".svg-cont",
 
 )
 svgImgTL.to(".svg-cont", {
-    y: -440,
-    duration: 1.5,
+    // y: -440,
+    duration: 1,
     // delay: 3,
     ease: "power2.inOut",
 })
 
 svgImgTL.to(".svg-cont", {
     y: 0,
-    duration: 1.3,
+    duration: 1.2,
     // delay: 4,
     ease: CustomEase.create("custom", "M0,0 C0.164,-0.017 0.181,0 0.265,0.094 0.336,0.174 0.44,0.379 0.472,0.519 0.492,0.609 0.567,0.813 0.63,0.907 0.705,1.018 0.704,1 1,1 "),
     ease: "power3.inOut",
