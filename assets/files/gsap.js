77c9ac1a334443033264d622352728efee990883
Dani-8 <daniyalw6190@gmail.com>
2025-07-14 22:04:33 +0500

    updated

--- assets/files/gsap.js
+++ assets/files/gsap.js
@@ -6,15 +6,36 @@ gsap.from('.main-content', {
 })
 
 gsap.fromTo(".svg-cont", 
-    {y: -400},
     {
-        y: 
+        y: -400,
+        duration: 1.5,
+        opacity: 0,
+        scale: .2,
+    },
+
+    {
+        scale: 1,
+        delay: 1.5,
+        duration: 1.5,
+        opacity: 1,
 
     }
 
 
 )
+gsap.to(".svg-cont", {
+    y: -440,
+    duration: 1.5,
+    delay: 3,
+    ease: "power2.inOut",
+})
 
+gsap.to(".svg-cont", {
+    y: 0,
+    duration: 1.5,
+    delay: 4,
+    ease: "power3.inOut",
+})
 
 
 
