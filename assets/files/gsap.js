164841265d4550b22076a363d316c68a11b82c1f
Dani-8 <daniyalw6190@gmail.com>
2025-07-14 22:25:50 +0500

    updated

--- assets/files/gsap.js
+++ assets/files/gsap.js
@@ -1,21 +1,23 @@
 gsap.from('.main-content', {
     opacity: 0,
-    duration: 50,
+    duration: 5,
 
 
 })
 
-gsap.fromTo(".svg-cont", 
+
+var svgImg = gsap.timeline()
+svgImg.fromTo(".svg-cont", 
     {
         y: -400,
-        duration: 1.5,
+        duration: .5,
         opacity: 0,
         scale: .2,
     },
 
     {
         scale: 1,
-        delay: 1.5,
+        delay: .5,
         duration: 1.5,
         opacity: 1,
 
@@ -23,18 +25,21 @@ gsap.fromTo(".svg-cont",
 
 
 )
-gsap.to(".svg-cont", {
+svgImg.to(".svg-cont", {
     y: -440,
     duration: 1.5,
-    delay: 3,
+    // delay: 3,
     ease: "power2.inOut",
 })
 
-gsap.to(".svg-cont", {
+svgImg.to(".svg-cont", {
     y: 0,
-    duration: 1.5,
-    delay: 4,
+    duration: 1.3,
+    // delay: 4,
+    ease: CustomEase.create("custom", "M0,0 C0.164,-0.017 0.181,0 0.265,0.094 0.336,0.174 0.44,0.379 0.472,0.519 0.492,0.609 0.567,0.813 0.63,0.907 0.705,1.018 0.704,1 1,1 "),
     ease: "power3.inOut",
+
+
 })
 
 
