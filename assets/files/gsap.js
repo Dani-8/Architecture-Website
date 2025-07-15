9a19915028753027500317e7814c938a2dd4b18c
Dani-8 <daniyalw6190@gmail.com>
2025-07-15 23:13:14 +0500

    üpdated

--- assets/files/gsap.js
+++ assets/files/gsap.js
@@ -59,8 +59,9 @@ svgImgTL.fromTo(".svg-cont",
     {
         scale: 1,
         delay: .5,
-        duration: 1.5,
+        duration: .8,
         opacity: 1,
+        ease: "power2.inOut",
 
     }
 
@@ -70,18 +71,20 @@ svgImgTL.to(".svg-cont", {
     y: -440,
     duration: 1,
     // delay: 3,
-    ease: "power2.inOut",
+    ease: "power3.inOut",
 })
 
 svgImgTL.to(".svg-cont", {
     y: 0,
-    duration: 1.2,
+    duration: 1,
     // delay: 4,
     ease: CustomEase.create("custom", "M0,0 C0.164,-0.017 0.181,0 0.265,0.094 0.336,0.174 0.44,0.379 0.472,0.519 0.492,0.609 0.567,0.813 0.63,0.907 0.705,1.018 0.704,1 1,1 "),
     ease: "power3.inOut",
 
 
 })
+
+
 
 var masterTL = gsap.timeline();
 masterTL.add(svgImgTL).add(mainContentText); // This order makes svgImgTL first
